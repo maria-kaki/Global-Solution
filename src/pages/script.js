6d5b58bd6327903785d37f4d2c6eecd4f67de933
maria-kaki <maria.oliveira170922@gmail.com>
2024-06-07 16:27:29 -0300

    login index e js

---
+++ src/pages/script.js
@@ -0,0 +1,17 @@
+// função do Login de usuário
+
+function login() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario !== "user") {
+        alert("Usuário incorreto!");
+    }
+    else if(senha !== "senha1234"){
+        alert("Senha incorreta!");
+    }
+    else {
+        alert(`Bem-vindo, ${usuario}!`);
+        window.location.href = "index.html";
+    }
+}
